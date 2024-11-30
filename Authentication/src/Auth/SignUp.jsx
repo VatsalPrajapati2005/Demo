ae16f41f589a10e67c6b30192ce8e2404d7861f2
VatsalPrajapati2005 <kamleshprajapti101947@gmail.com>
2024-11-30 09:17:52 +0530

    Add files via upload

---
+++ Authentication/src/Auth/SignUp.jsx
@@ -0,0 +1,50 @@
+// Shree Ganeshay namah 
+import { getAuth } from "@firebase/auth"
+import { useForm } from "react-hook-form"
+import { createUserWithEmailAndPassword } from "@firebase/auth"
+import auth from "./Firebase"
+function Singup(){
+
+    const { register, handleSubmit } = useForm()
+
+    function signup(data){
+        const {email , password } = data
+
+        createUserWithEmailAndPassword(auth,email,password)
+        .then((res)=>{
+            console.log(res)
+            console.log(res.user)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+
+    return(
+
+        <>
+            <div>
+            <h1>signup</h1>
+            <form action="" method="post" onSubmit={handleSubmit(signup)} className='col-4 mx-auto my-5 p-5 shadow'  >
+                <div className="my-3">
+                    <input type="text" {...register('email')} placeholder='enter email' className='form-control' />
+                </div>
+                <div className="my-3">
+                    <input type="password" {...register('password')} placeholder='enter password' className='form-control' />
+                </div>
+                {/* <div className="my-3">
+                    <button className='btn btn-danger'>signup</button>
+                    <NavLink to="/login">
+                        <button className='btn btn-success mx-2'>Login</button>
+                    </NavLink>
+
+                </div> */}
+                <button>submite</button>
+            </form>
+        </div>
+        
+        </>
+    )
+}
+
+export default Singup
